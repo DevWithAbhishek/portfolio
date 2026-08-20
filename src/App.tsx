7f706337a37213505be20383df007462d60ce180
Abhishek91K <abhishek@codewithabhishek.in>
2026-08-20 10:29:53 +0530

    feat: update the page to latest info

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import KridhaPage from "./pages/projects/Kridha";
+import ShelfAPIPage from "./pages/projects/Shelfapi";
+import IMSPage from "./pages/projects/Ims";
+import EngineeringPage from "./pages/Engineering";
 import { Preloader } from "./components/Preloader";
 
 const queryClient = new QueryClient();
@@ -17,14 +21,18 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         {isLoading && <Preloader onComplete={() => setIsLoading(false)} />}
-        <div 
-          className={`transition-opacity duration-500 ${isLoading ? 'opacity-0' : 'opacity-100'}`}
+        <div
+          className={`transition-opacity duration-500 ${isLoading ? "opacity-0" : "opacity-100"}`}
         >
           <Toaster />
           <Sonner />
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Index />} />
+              <Route path="/projects/kridha" element={<KridhaPage />} />
+              <Route path="/projects/shelfapi" element={<ShelfAPIPage />} />
+              <Route path="/projects/ims" element={<IMSPage />} />
+              <Route path="/engineering" element={<EngineeringPage />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
